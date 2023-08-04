785c54ae2b393b6e2e4ebb331e8949f84522d265
ggomez0 <gaspargomez2000@outlook.com>
2023-08-04 00:09:26 -0300

    Nombre y tipo de feriado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'
 
-const getURL = year => `https://nolaborables.com.ar/api/v2/feriados/${year}`;
+const getURL = year => `https://nolaborables.com.ar/api/v2/feriados/${year}`
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [nextHoliday, setNextHoliday] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const [nextHoliday, setNextHoliday] = useState(null)
 
   useEffect(() => {
     const now = new Date();
@@ -38,18 +39,22 @@ function App() {
       const nextHolidayDate = new Date(now.getFullYear(), nextHoliday.mes - 1, nextHoliday.dia);
       const differenceInMilliseconds = nextHolidayDate.getTime() - now.getTime();
       const differenceInDays = Math.round(differenceInMilliseconds / (1000 * 60 * 60 * 24));
-      return differenceInDays;
+      return { name: nextHoliday.motivo, days: differenceInDays, url: nextHoliday.info, tipo: nextHoliday.tipo}
     }
     return null;
   }
 
   return (
-    <div>
+    <div className='container'>
       {loading ? <h1>Loading...</h1> :
-        <h1>Quedan {getDifferenceInDays()} días para el próximo feriado</h1>
+        <>
+          <h1>Quedan {getDifferenceInDays().days} días para el próximo feriado</h1>
+          <p>Feriado {getDifferenceInDays().tipo} </p>
+          <p><a href={getDifferenceInDays().url}>{getDifferenceInDays().name}</a> </p>
+        </>
       }
     </div>
   );
 }
 
-export default App;
+export default App
